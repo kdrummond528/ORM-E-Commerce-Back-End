87e4579355fb6302105b6777b9eb6165d10a2fc4
Karina Drummond <k.drummond528@gmail.com>
2022-12-08 15:29:37 -0800

    modified tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -61,6 +61,18 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: { id: req.params.id }
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'There is no category under with this ID.' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
