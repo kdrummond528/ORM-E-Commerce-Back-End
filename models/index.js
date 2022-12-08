6b7ebf991998481bdad4072a68657e1fca4f5589
Karina Drummond <k.drummond528@gmail.com>
2022-12-08 15:08:17 -0800

    modified index.js

--- models/index.js
+++ models/index.js
@@ -18,6 +18,12 @@ Category.hasMany(Product, {
 })
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+})
 
 // Tags belongToMany Products (through ProductTag)
 
