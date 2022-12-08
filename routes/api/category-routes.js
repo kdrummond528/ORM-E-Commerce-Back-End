845f3bd83925885350b6bf1e49292ac2f9d95b1b
Karina Drummond <k.drummond528@gmail.com>
2022-12-08 15:17:53 -0800

    modified category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,20 +16,32 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "There is no category under with this ID." })
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 
